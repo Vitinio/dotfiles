4abc17d7eacde0f672c2a7ccaf9b039ffaa845cb
Vitinio <vitusauer@outlook.de>
2025-05-24 13:29:11 +0200

    -

---
+++ fullinstall.sh
@@ -0,0 +1,8 @@
+# Essentials
+yay -S wofi waybar neofetch nvim swaync swww python-pywal16 hyprpaper stow 
+
+# Toys
+yay -S asciiquarium cmatrix bash-pipes cowsay cbonsai
+
+# Optional
+yay -S phpstorm openvpn openssh bitwarden solaar
